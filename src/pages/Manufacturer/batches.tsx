a0ea354812b5719e5af305453d3dafeb9601cccc
Muhammad Ali <malishoaib.2001@gmail.com>
2024-04-09 00:13:03 +0500

    Batch Progress Map added

--- src/pages/Manufacturer/batches.tsx
+++ src/pages/Manufacturer/batches.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import BatchTable from '../../components/BatchTable'
 import Header from '../../components/Header'
 import { Container, Grid } from '@mui/material'
+import { BatchProgress } from '../../components/batchProgress'
 
 export default function Batches() {
     return (
@@ -11,6 +12,7 @@ export default function Batches() {
                 <Grid container>
                     <Grid item md={12}>
                         <BatchTable />
+                        <BatchProgress/>
                     </Grid>
                 </Grid>
             </Container>
