dfd5121135416f4922c9badf0145594e4415f322
Muhammad Ali <malishoaib.2001@gmail.com>
2024-07-07 19:30:02 +0500

    Database Connection Updated

--- models/index.js
+++ models/index.js
@@ -10,11 +10,7 @@ import { Notification } from "./Notification.js";
 const pass = process.env.VITE_DB_PASS;
 
 (async () => {
-  mongoose.connect(
-    // `mongodb://127.0.0.1:27017/fyp?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.0.0`
-    `mongodb://127.0.0.1:27017/fypsheet`
-    // `mongodb+srv://Umair:${pass}@cluster0.c6tj13p.mongodb.net/?retryWrites=true&w=majority`
-  );
+  mongoose.connect(process.env.DB_CONNECTION_STRING);
 })();
 
 export const db = {
